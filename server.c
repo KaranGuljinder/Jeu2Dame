b03a3845d3f3c8638b0abaf7a6437776e973e556
karanguljinder@gmail.com <karanguljinder@gmail.com>
2018-06-27 12:21:21 +0200

    Sever send message to client

--- server.c
+++ server.c
@@ -12,12 +12,28 @@
 #define BUFSIZE 1024
 extern int CPT = 0;
 extern int CLIENT_MAX = 2;
- 
+extern char *send_buf = "Bonjour";
+extern int lobby[5][5] = {{0}};
+extern int partie = 0;
+extern int joueur = 0;
+
+
 void send_to_all(int j, int i, int sockfd, int nbytes_recvd, char *recv_buf, fd_set *master)
 {
 	if (FD_ISSET(j, master)){
 		if (j != sockfd && j != i) {
 			if (send(j, recv_buf, nbytes_recvd, 0) == -1) {
+				perror("send");
+			}
+		}
+	}
+}
+
+void server_send(int j, int sockfd, fd_set *master)
+{		
+	if (FD_ISSET(j, master)){
+		if (j != sockfd) {
+			if (send(j, send_buf, BUFSIZE, 0) == -1) {
 				perror("send");
 			}
 		}
@@ -47,7 +63,8 @@ void send_recv(int i, fd_set *master, int sockfd, int fdmax)
 void connection_accept(fd_set *master, int *fdmax, int sockfd, struct sockaddr_in *client_addr){
 	
 	socklen_t addrlen;
-	int newsockfd;
+	int newsockfd, j;
+	
 	addrlen = sizeof(struct sockaddr_in);
 	if(CPT < CLIENT_MAX){		
 		if((newsockfd = accept(sockfd, (struct sockaddr *)client_addr, &addrlen)) == -1) {
@@ -62,9 +79,27 @@ void connection_accept(fd_set *master, int *fdmax, int sockfd, struct sockaddr_i
 			if(CPT == 1){
 				printf("Premier joueur connecté : %s on port %d \n",inet_ntoa(client_addr->sin_addr), ntohs(client_addr->sin_port));
 				printf("Attente du deuxième joueur !\n");
+				joueur++;
+				lobby[joueur][partie] = newsockfd;
+				
 			}else if(CPT == 2){
 				printf("Deuxième joueur connecté : %s on port %d \n",inet_ntoa(client_addr->sin_addr), ntohs(client_addr->sin_port));
-				printf("Début du jeu ! \n");			
+				printf("Début du jeu ! \n");
+				// lancement du jeu 
+				joueur++;
+				lobby[joueur][partie] = newsockfd;
+				if(joueur == 2){
+				   	joueur = 0;
+					partie++;
+	                             
+				}
+				printf("Partie n°%d \n", partie);
+				CPT = 0;
+				for(j = 0; j <= *fdmax; j++){
+					server_send(j,sockfd,master);
+				}
+
+							
 			}
 		}
 	 }
